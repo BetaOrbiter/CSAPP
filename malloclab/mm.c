b8d9b716f7940206ad079605b7ecea9484bf81b0
李洲诚 <eto_li@outlook.com>
2021-07-16 16:16:10 +0800

    finish

--- malloclab/mm.c
+++ malloclab/mm.c
@@ -9,18 +9,17 @@
  * NOTE TO STUDENTS: Replace this header comment with your own header
  * comment that gives a high level description of your solution.
  */
-/****************************************************************************/
-/*                                                                          */
-/*                                                                          */
-/*                                 还妹有做出来                              */
-/*                                  先放一放                                 */
-/*                                  下次一定                                 */
-/****************************************************************************/
 
+/****************************************************************/
+/*                         与课本不同                            */
+/*                  显式空闲链表带头结点，放在堆头                 */
+/*              使用了双链表结构体,链表操作方便                    */
+/*              不使用指向负载的指针，只在接口处转换                */
+/*         所有大小均为整块大小(而非负载)，亦在接口处转换            */
+/*                  一定要时刻注意这两种区别                       */
+/****************************************************************/
 #include <stdio.h>
-#include <stdlib.h>
-#include <assert.h>
-#include <unistd.h>
+#include <stdint-gcc.h>
 #include <string.h>
 
 #include "mm.h"
@@ -52,318 +51,252 @@ team_t team = {
 
 #define SIZE_T_SIZE (ALIGN(sizeof(size_t)))
 
-#define WSIZE 4U
-#define DSIZE 8U
-#define CHUNKSIZE (1U<<12)
-#define MINBLOCKSIZE ALIGN((DSIZE + 2*sizeof(void*))) //最小块大小（空闲块头尾，指针）
-#define FRFLAG 0x0U
-#define ALFLAG 0x1U
-#define PREALL 0x2U
+#define LIST_MAX  (10)                              //空闲链表数量
+#define WSIZE (4)                                   //字大小
+#define DSIZE (8)                                   //双字大小
+#define CHUNKSIZE (1<<12)                           //页大小
+#define PTR_SIZE (sizeof(void*))                    //显式空闲链表指针大小
+#define MIN_BLK_SIZE (ALIGN(2*WSIZE+2*PTR_SIZE))    //最小块大小(最小空闲块所需大小)
+#define FRFLAG (0x0)                                //空闲块标志
+#define ALFLAG (0x1)                                //已分配标志
+#define PREALL (0x2)                                //前方已分配标志
 
-#define MAX(x, y) ( ((x) > (y)) ? (x) : (y) )
-#define MIN(x, y) ( ((x) < (y)) ? (x) : (y) )
+#define GET(head)           (*(unsigned*)(head))
+#define GET_SIZE(head)      (GET(head) & ~0x3)
+#define GET_MASK(head)      (GET(head) & 0x3)
+#define GET_ALLOC(head)     (GET(head) & ALFLAG)
+#define GET_PRE_ALLOC(head) (GET(head) & PREALL)
+#define PACK(size, mask)    ((size)|(mask))
+#define PUT(ptr, val)       ((*(unsigned*)(ptr)) = (val))
 
-#define GET(p) (*(unsigned *)(p))               //get the word *p
-#define PUT(p, val) (*(unsigned *)(p) = (val))  //make word *p = val
-#define PACK(size, flag) ((size) | (flag))    //pack the block size and allocated bit
+#define HEAD(playload_ptr)      ((char*)(playload_ptr) - WSIZE)                   //负载-》头部
+#define LOAD(head)              ((char*)(head) + WSIZE)                           //头部-》负载
+#define FOOT(free_head)         (NEXT_HEAD(free_head) - WSIZE)                  //头部-》尾部
+#define NEXT_HEAD(head)         ((char*)(head) + GET_SIZE(head))                  //头部-》下一个块头部
+#define PREV_HEAD(head)         ((char*)(head) - GET_SIZE((char*)head-WSIZE))                  //头部-》上一个块头部（必须是空闲）
 
-#define GET_SIZE(p) (GET(p) & ~0x7)             //get size of block p
-#define GET_ALLOC(p) (GET(p) & ALFLAG)          //if block p allocated
-#define GET_PREV_ALLOC(p) (GET(p) & PREALL)
+#define MIN(x,y)        (((x)<(y))?(x):(y))
+#define MAX(x,y)        (((x)<(y))?(y):(x))
 
-#define HDRP(pp) ((char *)(pp) - WSIZE)//get header from address of play_load
-#define FTRP(pp) (HDRP(pp) - WSIZE + GET_SIZE(HDRP(pp)))//get foot from address of play_load
-#define NEXT_BLKP(p) (((char *)(p)) + GET_SIZE(p))//get next 
-#define PREV_BLKP(p) (((char *)(p)) - GET_SIZE(((char *)(p) - WSIZE)))
+typedef struct node{
+    //为链表操作方便，将头部强转为双链表结构体
+    //题目不允许但我实在是受不了了
+    unsigned block_size_mask;
+    void *prev_node;
+    void *next_node;
+}node;
+static char * heap_listp = NULL;
+static node *node_listp  = NULL;
 
-#define NEXT_PTR(p) (*(void **) ((char *)(p)+WSIZE + sizeof(void*)))
-#define PREV_PTR(p) (*(void **) ((char *)(p)+WSIZE))
-#define LISTNUM 16
-static char *heap_listp = 0;
-static void *segregated_free_list[LISTNUM];
+static void* extend_heap(size_t block_size);        //扩展堆大小，失败返回NULL,成功返回新空块头地址
+static void* coalesce(void *head);                  //合并前后可能的空闲块，返回合并后头地址
+static void  place(void *head, size_t block_size);  //分割空闲块，得到忙碌块和剩余部分(如有),不检查是否足够
 
-//内部一律处理头部指针，整个块大小，只在输入输出时转换
+//链表操作
+static node* find_fit(size_t block_size);       //返回大于参数的最小空闲块，失败返回NULL
+static void delete(struct node* ptr);           //从链表中删除节点
+static void insert(struct node* ptr);           //把节点插入链表
 
-static void *extend_heap(size_t size);//
-static void *coalesce(void *ptr);
-static void place(void *ptr, int size);
-
-static inline void *find_fit(size_t size);//malloc 中查找合适的块
-static void insert_block(void *ptr);//向链表组插入块
-static void delete_block(void *ptr);//从链表组删除块
-static inline size_t lowest_list(size_t size);//返回合适的链表
-static void checker(void);
-/* 
- * mm_init - initialize the malloc package.
- */
-int mm_init(void)
+int mm_init (void)
 {
-    for (int i = 0; i < LISTNUM; i++)
-        segregated_free_list[i] = NULL;
-    heap_listp = mem_sbrk(4*WSIZE);
-    if((void*)-1 == heap_listp)
+    if(-1 == (node_listp = mem_sbrk(4*WSIZE + LIST_MAX * sizeof(node))))
         return -1;
     
-    PUT(heap_listp, 0);
-    PUT(heap_listp + WSIZE, PACK(8,ALFLAG));
-    PUT(heap_listp + 2*WSIZE, PACK(8,ALFLAG));
-    PUT(heap_listp + 3*WSIZE, PACK(0,ALFLAG|PREALL));
-    heap_listp += DSIZE;
+    for(size_t i=0;i<LIST_MAX-1;i++){
+        node_listp[i].block_size_mask = (MIN_BLK_SIZE<<i);
+        node_listp[i].prev_node = NULL;
+        node_listp[i].next_node = NULL;
+    }
+    node_listp[LIST_MAX-1].block_size_mask = UINT32_MAX;//最后的链表无穷大
+    node_listp[LIST_MAX-1].prev_node = NULL;
+    node_listp[LIST_MAX-1].next_node = NULL;
 
-    if (NULL == extend_heap(CHUNKSIZE))
+    heap_listp = node_listp+LIST_MAX;
+    heap_listp += WSIZE;
+    PUT(heap_listp, PACK(8,ALFLAG|PREALL));
+    PUT(heap_listp+WSIZE, PACK(8,ALFLAG|PREALL));
+    PUT(heap_listp+DSIZE, PACK(0, ALFLAG|PREALL));
+    if(NULL == extend_heap(CHUNKSIZE))
         return -1;
-    else
-        return 0;
+    return 0;
 }
-
-/* 
- * mm_malloc - Allocate a block by incrementing the brk pointer.
- *     Always allocate a block whose size is a multiple of the alignment.
- */
-void *mm_malloc(size_t size)
+void* mm_malloc (size_t size)
 {
-    if (0 == size)
+    if(0 == size)
         return NULL;
-
-    size_t real_size = ALIGN(size + DSIZE);//加头部之后八字节对其
-    real_size = MAX(real_size, MINBLOCKSIZE);//no smaller than minimum free block
-    void* ptr;
-
-    if (NULL == (ptr = find_fit(real_size)))
-        if(NULL == (ptr = extend_heap(MAX(real_size, CHUNKSIZE))))
+    size_t block_size = ALIGN(size+WSIZE);
+    block_size = MAX(block_size, MIN_BLK_SIZE);
+    void * bp = find_fit(block_size);
+    if(NULL == bp)
+        if(NULL == (bp = extend_heap(MAX(block_size,CHUNKSIZE))))
             return NULL;
-    place(ptr, real_size);
-    checker();
-    return ptr + WSIZE;//return address of play_load instead of header
+        else
+            bp = find_fit(block_size);
+    place(bp, block_size);
+
+    return bp+WSIZE;
 }
-
-/*
- * mm_free - Freeing a block does nothing.
- */
-void mm_free(void *bp)
-{
-    void* const header = HDRP(bp);//change to header pointer
-    const unsigned val = GET(header) & ~ALFLAG;
-    PUT(HDRP(bp), val);
-    PUT(FTRP(bp), val);
-
-    // insert_block(header);
-    coalesce(header);
-    checker();
-}
-
-/*
- * mm_realloc - Implemented simply in terms of mm_malloc and mm_free
- */
-void *mm_realloc(void *ptr, size_t size)
+void  mm_free (void *ptr)
 {
     if(NULL == ptr)
-        return NULL;
+        return ;
+    void * const head = HEAD(ptr);
+    PUT(head, GET(head)&~ALFLAG);
+    PUT(FOOT(head), GET(head));
+    void * const next_head = NEXT_HEAD(head);
+    PUT(next_head, GET(next_head)&~PREALL);
+    if(!GET_ALLOC(next_head))
+        PUT(FOOT(next_head), GET(next_head));
+    coalesce(head);
+}
+void* mm_realloc(void *ptr, size_t size)
+{
     if(0 == size){
         mm_free(ptr);
         return NULL;
     }
-
-    void * const header = HDRP(ptr);//change to header pointer    
-    size_t real_size = ALIGN(size+DSIZE);
-    real_size = MAX(real_size, MINBLOCKSIZE);
-    const size_t original_size = GET_SIZE(header);
-
-    if (original_size >= real_size) {
-        place(HDRP(ptr), real_size);
+    if(size + WSIZE <= GET_SIZE(HEAD(ptr)))//新负载加头部小于原块大小
         return ptr;
-    }
-    else if(GET_ALLOC(NEXT_BLKP(header)) || (GET_SIZE(NEXT_BLKP(header)) < real_size-original_size)){
-        void* new_playload_p = mm_malloc(size);
-        memcpy(new_playload_p, ptr, original_size-WSIZE);
-        mm_free(ptr);
-        return new_playload_p;
-    }
-    else{
-        delete_block(NEXT_BLKP(header));
-        GET(header)+=GET_SIZE(NEXT_BLKP(header));
-        GET(NEXT_BLKP(NEXT_BLKP(header))) = GET(NEXT_BLKP(NEXT_BLKP(header))) | PREALL;
-        return ptr;
-    }
-    checker();
-}
-
-void* extend_heap(size_t size)
-{
-    size = ALIGN(size);
-    void* ptr = mem_sbrk(size);
-    if ((void *)-1 == ptr)
-        return NULL;
-
-    //注意原先的结束块，因此ptr指向新的负载
-    PUT(HDRP(ptr), PACK(size, FRFLAG|GET_PREV_ALLOC(HDRP(ptr))));//注意原结束块保存的，原最后一个块的free/allocated信息
-    PUT(FTRP(ptr), GET(HDRP(ptr)));
-    PUT(NEXT_BLKP(HDRP(ptr)), PACK(0, ALFLAG));
     
-    // insert_block(HDRP(ptr));
-    return coalesce(HDRP(ptr));
-}
-
-void* coalesce(void* ptr)
-{
-
-    void * const pre_head = PREV_BLKP(ptr);
-    void * const next_head = NEXT_BLKP(ptr);
-    const size_t prev_alloc = GET_PREV_ALLOC(ptr);
-    const size_t next_alloc = GET_ALLOC(next_head);
-
-    if(prev_alloc && next_alloc)
-        ;
-    else if (!prev_alloc && next_alloc) {
-        delete_block(pre_head);
-
-        const size_t val = GET_SIZE(ptr) + GET(pre_head);//注意保留前一个块的PREALL
-        PUT(pre_head,  val);
-        PUT(FTRP(ptr + WSIZE), val);
-        ptr = pre_head;
-    }
-    else if (prev_alloc && !next_alloc) {
-        delete_block(next_head);
-
-        const size_t val = GET(ptr) + GET_SIZE(next_head);//同上
-        PUT(ptr, val);
-        PUT(FTRP(ptr + WSIZE), val);
+    void * const head = HEAD(ptr);
+    void * const next_head = NEXT_HEAD(head);
+    if(GET_ALLOC(NEXT_HEAD(head)) || 
+    (size+WSIZE > GET_SIZE(head)+GET_SIZE(next_head))){
+        void *new_ptr = mm_malloc(size);
+        memcpy(new_ptr, ptr, GET_SIZE(head)-WSIZE);
+        mm_free(ptr);
+        return new_ptr;
     }
     else{
-        delete_block(pre_head);
-        delete_block(next_head);
-
-        const size_t val = GET(pre_head) + GET_SIZE(ptr) + GET_SIZE(next_head);
-        PUT(pre_head, val);
-        PUT(FTRP(next_head + WSIZE), val);
-        ptr = pre_head;
+        const size_t new_block_size = MAX(ALIGN(size+WSIZE), MIN_BLK_SIZE);
+        const size_t require_size = new_block_size - GET_SIZE(head);
+        place(next_head, require_size);
+        PUT(head, PACK(new_block_size, GET_MASK(head)));
+        return ptr;
     }
-    insert_block(ptr);
-    return ptr;
 }
 
-void place(void* ptr, int size)
-{
-    delete_block(ptr);
-    const size_t prev_size = GET_SIZE(ptr);
-    const size_t prev_alloc = GET_PREV_ALLOC(ptr);
-    if (prev_size >= MINBLOCKSIZE + size) {
-        PUT(ptr, PACK(size, ALFLAG | prev_alloc));
-        ptr = NEXT_BLKP(ptr);
-        PUT(ptr, PACK(prev_size - size, FRFLAG | PREALL));
-        PUT(FTRP(ptr+WSIZE), PACK(prev_size - size, FRFLAG | PREALL));
-        insert_block(ptr);
+void* extend_heap(size_t block_size){
+    block_size = ALIGN(block_size);
+    block_size = MAX(block_size,MIN_BLK_SIZE);
+    void *bp;
+    if(-1 == (bp = mem_sbrk(block_size)))
+        return NULL;
+    
+    bp = HEAD(bp);
+    PUT(bp,             PACK(block_size,GET_PRE_ALLOC(bp)));
+    PUT(FOOT(bp),       PACK(block_size,GET_PRE_ALLOC(bp)));
+    PUT(NEXT_HEAD(bp),  PACK(0,ALFLAG));
+
+    return coalesce(bp);
+}
+
+void* coalesce(void* head){
+    unsigned block_size = GET_SIZE(head);
+    unsigned mask = GET(head) & 0x3;
+
+    if(!GET_ALLOC(NEXT_HEAD(head))){
+        delete((node*)NEXT_HEAD(head));
+        block_size += GET_SIZE(NEXT_HEAD(head));
+    }
+    if(!GET_PRE_ALLOC(head)){
+        delete((node*)PREV_HEAD(head));
+        block_size += GET_SIZE(PREV_HEAD(head));
+        mask       |= GET_PRE_ALLOC(PREV_HEAD(head));
+        head        = PREV_HEAD(head);
+    }
+    PUT(head,       PACK(block_size, mask));
+    PUT(FOOT(head), PACK(block_size, mask));
+    insert((node*)head);
+
+    return head;
+}
+
+void  place(void *head, size_t block_size){
+    //to speed up, i use few marcos
+    delete((node*)head);
+    const unsigned content = GET(head);
+    const size_t left_size = (content&~0x3) - block_size;
+    if(left_size < MIN_BLK_SIZE){
+        PUT(head, PACK(content,ALFLAG));
+        void * const next_head = (char*)head + (content&~0x3);
+        const unsigned next_content = GET(next_head);
+        PUT(next_head,PACK(next_content, PREALL));
+        if(!(next_content&ALFLAG))
+            PUT((char*)next_head + (next_content&~0x3) - WSIZE, content);
     }
     else{
-        PUT(ptr, PACK(prev_size, ALFLAG | prev_alloc));
-        ptr = NEXT_BLKP(ptr);
-        PUT(ptr, GET(ptr) | PREALL);
+        PUT(head, PACK(block_size,(PREALL&content)|ALFLAG));
+        void * const next_head = (char*)head + block_size;
+        PUT(next_head,                          PACK(left_size, PREALL));
+        PUT((char*)next_head+left_size-WSIZE,   PACK(left_size, PREALL));
+        insert((node*)next_head);
     }
 }
 
-inline void* find_fit(size_t size)
-{
-    void* ptr = NULL;
-    for (size_t index = lowest_list(size); index < LISTNUM; index++) {
-        void* list = segregated_free_list[index];
-        for (ptr = list; (NULL != ptr) && (GET_SIZE(ptr) < size);)
-            ptr = NEXT_PTR(ptr);
-        if (NULL != ptr)
+inline void delete(node* ptr){
+    node * const pre = ptr->prev_node;
+    node * const nxt = ptr->next_node;
+    pre->next_node = nxt;
+    if(NULL != nxt)
+        nxt->prev_node = pre;
+}
+
+void insert(struct node* ptr){
+    //to speed up, i use few marcos
+    node *pre, *nxt;
+    const unsigned block_size = GET_SIZE(ptr);
+    for(pre=node_listp;(pre->block_size_mask&~0x3) <block_size;pre++)
+        continue;//确定放在哪个链表，注意头节点size字段无掩码而ptr有
+    nxt = pre->next_node;
+    //确定节点位置
+    while(NULL!=nxt && (nxt->block_size_mask&~0x3)<block_size){
+        pre = nxt;
+        nxt = nxt->next_node;
+    }
+    pre->next_node = ptr;
+    ptr->prev_node = pre;
+    ptr->next_node = nxt;
+    if(NULL !=  nxt)
+        nxt->prev_node = ptr;
+}
+
+node* find_fit(size_t block_size){
+    node * ret = NULL;
+    size_t i;
+    for(i=0;node_listp[i].block_size_mask < block_size && i<LIST_MAX; i++)
+        continue;//筛掉不可能的链表组
+    
+    for(;i<LIST_MAX;i++){
+        node * const head = node_listp + i;
+        for(node*ptr = head->next_node; NULL!=ptr; ptr=ptr->next_node)
+            if((ptr->block_size_mask&~0x3) >= block_size){
+                ret = ptr;
+                break;
+            }
+        if(NULL != ret)
             break;
     }
-    return ptr;
+    return ret;
 }
-
-void insert_block(void* ptr)
-{
-    const size_t size = GET_SIZE(ptr);
-    void** list = &segregated_free_list[lowest_list(size)];
-    void *pre = *list;
-    void* nxt = pre;
-    
-    while ((NULL != nxt) && (GET_SIZE(nxt) < size)) {
-        pre = nxt;
-        nxt = NEXT_PTR(nxt);
-    }
-
-    if(NULL == pre){
-        //没有节点
-        *list = ptr;
-        NEXT_PTR(ptr) = NULL;
-        PREV_PTR(ptr) = NULL;
-    }else{ 
-        if(nxt == *list){
-            //第一个插入
-            *list = ptr;
-            PREV_PTR(ptr) = NULL;
-        }
-        else{
-            NEXT_PTR(pre) = ptr;
-            PREV_PTR(ptr) = pre;
-        }
-        NEXT_PTR(ptr) = nxt;
-        if(NULL != nxt)
-            PREV_PTR(nxt) = ptr;
-    }
-}
-
-void delete_block(void* ptr)
-{
-    void* pre = PREV_PTR(ptr);
-    void* nxt = NEXT_PTR(ptr);
-    if (NULL == pre)
-        //注意ptr是第一个节点的情况
-        for(size_t index = lowest_list(GET_SIZE(ptr)); index<LISTNUM; index++)
-            if(segregated_free_list[index] == ptr)
-                segregated_free_list[index] = nxt;
-            else
+static void mem_dump(int mode){
+    if(0 == mode){
+        for(int i=0;i<LIST_MAX;i++){
+            if(NULL == node_listp[i].next_node)
                 continue;
-    else
-        NEXT_PTR(pre) = nxt;
-
-    if (NULL != nxt)
-        PREV_PTR(nxt) = pre;
-}
-
-inline size_t lowest_list(size_t size)
-{
-    size_t index = 0;
-    size >>= 3;
-    while ((index < LISTNUM - 1) && (size > 1))
-    {
-        size >>= 1;
-        index++;
+            printf("size between %u and %u\n", node_listp[i].block_size_mask>>1, node_listp[i].block_size_mask);
+            for(node *p=node_listp[i].next_node;p!=NULL;p=p->next_node)
+                printf("%p %-10u %-10p %-10p\n", p, (p->block_size_mask&~0x3) , p->prev_node, p->next_node);
+        }
     }
-    return index;
-}
-
-void checker(void){
-    if(*(unsigned*)0xf69d5f4c == 0x115b)
-        exit(0);
-    // if(*(char *)0xf69d70a0  == 0x5a)
-    //     ;
-    static size_t count = 0;
-    count++;
-    // int flg = 1;
-    // for(size_t index=0; index<LISTNUM; index++)
-    //     if(segregated_free_list[index] != NULL){
-    //         void *pre, *nxt;
-    //         pre = segregated_free_list[index];
-    //         if(PREV_PTR(pre)!=NULL)
-    //             flg = 0;
-
-    //         for(nxt = NEXT_PTR(pre); NULL!=nxt;pre =nxt, nxt=NEXT_PTR(nxt))
-    //             if((GET_SIZE(nxt)<GET_SIZE(pre)) || (PREV_PTR(nxt)!=pre) || (index!=lowest_list(GET_SIZE(pre))))
-    //                 flg = 0;
-    //         if(lowest_list(GET_SIZE(pre)) != index)
-    //             flg = 0;
-    //     }
-
-    // for(void *pre=NEXT_BLKP(HDRP(heap_listp)); GET(pre) != 0x1; pre=NEXT_BLKP(pre))
-    //     ;
-    // if(flg == 0)
-    // {
-    //     printf("error at %u\n", count);
-    //     exit(0);
-    // }
+    else{
+        for(void *p=heap_listp;0!=GET_SIZE(p);p=NEXT_HEAD(p)){
+            printf("%p size:%-5u %s pre_%s ",p , GET_SIZE(p), 
+                GET_ALLOC(p)?"busy":"free", GET_PRE_ALLOC(p)?"busy":"free");
+            if(0 == GET_ALLOC(p))
+                printf(GET(p)==GET(FOOT(p))?"foot right":"foot error");
+            putchar('\n');
+        }
+    }
+    puts("\n\n");
 }
